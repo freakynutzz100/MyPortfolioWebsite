6ce68c2d2684d15827cf8bdee04688f96d2ff0c8
freakynutzz100 <freetimerenderzz100@gmail.com>
2025-07-02 16:50:03 +0530

    message

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         port: 5173,
         proxy: {
-            '/api': process.env.VITE_SERVER_URL || 'http://localhost:8000',
+            '/api': process.env.VITE_SERVER_URI || 'http://localhost:8000',
         }
     },
     resolve: {
